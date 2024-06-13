779ee47b05460bae1af810f745179767aee68681
vitaliihair <vitobarbersochi@gmail.com>
2024-06-13 21:07:31 +0300

    сделана тряска кнопки

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// import { gsap } from 'gsap-core';
 ymaps.ready(Init);
 
 // let center = [43.5951976893704,39.717826278298716];
@@ -19,33 +20,31 @@ function Init(){
 
 }
 
-// ymaps.ready(init);
+const button = document.getElementById('btn');
+// const button = document.getElementById("myButton");
 
-// function init() {
-//     var myMap = new ymaps.Map('map', {
-//             center: [55.76, 37.64],
-//             zoom: 10
-//         }, {
-//             searchControlProvider: 'yandex#search'
-//         }),
 
-//     // Создаем геообъект с типом геометрии "Точка".
-//         myGeoObject = new ymaps.GeoObject({
-//             // Описание геометрии.
-//             geometry: {
-//                 type: "Point",
-//                 coordinates: [55.8, 37.8]
-//             },
-//             // Свойства.
-//             properties: {
-//                 // Контент метки.
-//                 iconContent: 'Я тащусь',
-//                 hintContent: 'Ну давай уже тащи'
-//             }
-//         }, {
-//             // Опции.
-//             // Иконка метки будет растягиваться под размер ее содержимого.
-//             preset: 'islands#blackStretchyIcon',
-//             // Метку можно перемещать.
-//             draggable: true
-//         }}
+function shakeButton() {
+    let button = document.getElementById('btn');
+    
+    // Трясем кнопку 4 раза
+    for (let i = 0; i < 4; i++) {
+      setTimeout(() => {
+        button.style.transform = 'translate(3px, 0)';
+      }, i * 100);
+      setTimeout(() => {
+        button.style.transform = 'translate(-3px, 0)';
+      }, (i * 100) + 50);
+      setTimeout(() => {
+        button.style.transform = 'translate(0, 0)';
+      }, (i * 100) + 100);
+    }
+  
+    // Задержка перед следующим тряском
+    setTimeout(() => {
+      shakeButton();
+    }, 2000);
+  }
+  
+  shakeButton();
+
